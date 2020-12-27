e5b0670aeea0ce209b0f015cce5b25a7389765cb
Rahul Mahajan <rahulmahajan98645@gmail.com>
2020-12-28 00:20:37 +0530

    adding main server!

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
-
+const bodyparser = require('body-parser')
 const app = express()
 
+app.use(bodyparser.urlencoded({extended:true}))
+
 app.get('/',function(req,res){
-    console.log(req);
-    res.send('<h1>this is rahul1111')
+    // res.send('hello this is rahul mahajan from adgitm aff to GGSIPU doing cse batch 2023')
+    res.sendFile(__dirname+'/weather.html')
 })
 
+app.post('/',function(req,res){
+    const location = req.body.place
+    console.log('location added by the user is '+location);
+})
 
-app.listen(777,function(){
-    console.log('this is rahul mahajan');
+app.listen(7777,function(){
+    console.log('server on port 7777');
 })
