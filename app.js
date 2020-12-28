228a3c5d19bd68630a60e9d5657caedd7772f475
Rahul Mahajan <rahulmahajan98645@gmail.com>
2020-12-28 13:46:11 +0530

    adding ejs

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ app.post('/',function(req,res){
             const descrip = weatherData.weather[0].description
             const icon = weatherData.weather[0].icon
             const imgurl = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
-            res.write('current temp of '+ locat +' is '+ temp+'°C')
-            res.write("today's maximum temp of "+ locat +" is "+max_temp+'°C')
-            res.write("today's minimum temp of "+ locat +"  is "+min_temp+'°C')
-            res.write("humidity in " + locat +" is: "+humid) 
-            res.write("current weather is "+descrip) 
+            res.write('<p> current temp of '+ locat +' is '+ temp+"°"+'C </p>')
+            res.write("<h2> today's maximum temp of "+ locat +" is "+max_temp+'°C </h2>')
+            res.write("<h1> today's minimum temp of "+ locat +"  is "+min_temp+'°C</h1>')
+            res.write("<h3>humidity in " + locat +" is: "+humid+"</h3>") 
+            res.write("<h3>current weather is "+descrip+"</h3>") 
             res.write("<img src="+imgurl+">")
             res.send()
         })
